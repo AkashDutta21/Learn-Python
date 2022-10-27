00df1ee36ae44aac3ec84bbb629edb70f6704502
Akash Dutta <adwork1998@gmail.com>
2022-10-28 01:16:23 +0530

    added useful string methods in stringMethods.py file

---
+++ stringMethods.py
@@ -0,0 +1,15 @@
+# important string methods
+name = "akash"
+number = "123"
+
+print(len(name))  # returns the length of the string
+print(name.find("k"))  # return the position of the character in the string
+print(name.capitalize())  # print the string with 1st letter capitalized
+print(name.upper())  # prints the string in all uppercase
+print(name.lower())  # prints the string in all lowercase
+print(number.isdigit())  # checks if string is a number or not
+print(name.isdigit())  # checks if string is a number or not
+print(name.isalpha())  # checks if the string consists of only alphabetical characters.
+print(name.count("a"))  # count the number of a particular character in the string
+print(name.replace("a", "l"))  # replace the  specific character in the string
+print(name * 3)  # print the name multiple times
