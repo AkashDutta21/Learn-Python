e8c627c34570eb320591b7f01b0b66dd78ca434b
Akash Dutta <adwork1998@gmail.com>
2022-10-28 00:45:23 +0530

    Added varaibles file

---
+++ variables.py
@@ -0,0 +1,28 @@
+# string variable
+name = "Akash"
+first_name = "Akash"
+last_name = "Dutta"
+print(type(name))
+print(first_name + " " + last_name)
+print("\n")
+
+# integer variable
+num = 10
+print(num)
+print(type(num))
+print("My age is : " + str(num) + "years")  # typecasting to string to perform concat
+print("\n")
+
+# float variable
+float_value = 10.5
+print(float_value)
+print(type(float_value))
+print("My height is : "+str(float_value)+"cm")  # typecasting to string to perform concat
+print("\n")
+
+# boolean variable
+isHuman = True
+print(isHuman)
+print(type(isHuman))
+print("Am I Human : " + str(isHuman))
+
