de98ee36cca4569e011225bd88805ff5826f461f
Akash Dutta <adwork1998@gmail.com>
2022-10-30 22:52:55 +0530

    added user Input program

---
+++ userInput.py
@@ -0,0 +1,7 @@
+name = input("What is your name?: ")
+age = int(input("What is your age?: "))
+height = float(input("What is your height?: "))
+
+print("Hello "+name)
+print("Age: "+str(age))
+print("Height: "+str(height)+"cm")
